5b0c23070062c817c33cab5b809e987ddae13fd3
Rudy Pelrine <drawncloser@gmail.com>
2023-07-28 14:22:54 -0400

    Fix a type

--- src/buildStrongRoute.tsx
+++ src/buildStrongRoute.tsx
@@ -10,10 +10,9 @@ import {
 import { useStrongLoaderData, useStrongRouteError } from "./hooks";
 import { strongResponse } from "./strongResponse";
 import {
+  BrandedDataFunction,
   BuildStrongRemixRouteExportsOpts,
   PickDataAndStatus,
-  StrongAction,
-  StrongLoader,
   StrongRedirect,
   StrongRemixRouteExports,
   StrongResponse,
@@ -50,9 +49,7 @@ const handleDataFunctionForRemix = async <
   Failure extends StrongResponse<unknown, NonRedirectStatus> = never,
   Redirect extends StrongRedirect<string, RedirectStatus> = never,
 >(
-  dataFunction:
-    | StrongLoader<Failure, Success, Redirect>
-    | StrongAction<Failure, Success, Redirect>,
+  dataFunction: BrandedDataFunction<Failure, Success, Redirect>,
   args: DataFunctionArgs,
 ) => {
   const resultEffect = (await dataFunction(args)) as Effect.Effect<
